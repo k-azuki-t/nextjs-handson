eeabdf0ec4c69c5d4e19dea72cee79f120565830
k-azuki-t <teraoctit@gmail.com>
2025-06-28 17:18:50 +0900

    37. useActionStateでサーバーのバリデーションをクライアントに表示

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -1,8 +1,22 @@
 'use server';
 import { redirect } from "next/navigation";
 import { ContactSchema } from "@/validations/contact";
+import { email } from "zod/v4";
 
-export async function submitContactForm(formData: FormData) {
+// ActionStateの型定義
+type ActionState = {
+    success: boolean;
+    errors: {
+        name?: string[];
+        email?: string[];
+    };
+    serverError?: string;
+}
+
+export async function submitContactForm(
+    prevState: ActionState,
+    formData: FormData
+    ): Promise<ActionState> {
     const name = formData.get('name');
     const email = formData.get('email');
 
@@ -10,9 +24,16 @@ export async function submitContactForm(formData: FormData) {
     const validationResult = ContactSchema.safeParse({name, email});
     if (!validationResult.success) {
         // バリデーションエラーがある場合は、エラーを返す
-        const errors = validationResult.error.flatten();
+        const errors = validationResult.error.flatten().fieldErrors;
         console.log('サーバー側でエラー', errors)
-        return {};
+        return {
+            success: false,
+            errors: {
+                name: errors.name || [],
+                email: errors.email || []
+            },
+            serverError: '入力内容にエラーがあります。'
+        };
     }
     // DB登録
 
